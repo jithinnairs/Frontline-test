0f782f8f93b6e7543cb2d0d81c8eae53ac550951
jithinnairs <33701182+jithinnairs@users.noreply.github.com>
2021-05-09 11:31:24 +0530

    Update BasicSimulationTwo.scala

--- maven-sample/src/test/scala/frontline/sample/BasicSimulationTwo.scala
+++ maven-sample/src/test/scala/frontline/sample/BasicSimulationTwo.scala
@@ -94,5 +94,5 @@ class BasicSimulationTwo extends Simulation {
 
  // setUp(scn.inject(atOnceUsers(1)).protocols(httpProtocol))
 setUp(
-    scn.inject(rampUsers(1) during (1 seconds))
+    scn.inject(rampUsers(2) during (2 seconds))
   ).protocols(httpProtocol)}
