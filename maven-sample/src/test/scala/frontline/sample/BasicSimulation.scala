ede0b80d6ecb971555b17df66e1f731f0f692038
jithinnairs <33701182+jithinnairs@users.noreply.github.com>
2021-05-13 22:52:34 +0530

    Update BasicSimulation.scala

--- maven-sample/src/test/scala/frontline/sample/BasicSimulation.scala
+++ maven-sample/src/test/scala/frontline/sample/BasicSimulation.scala
@@ -26,6 +26,6 @@ class BasicSimulation extends Simulation {
     )
 
   setUp(
-    scn.inject(rampUsers(50) during (50 seconds))
+    scn.inject(rampUsers(50) during (60 seconds))
   ).protocols(httpConf)
 }
