d4b1fa6cbba763bad587e3ab6d370a9f97619dcc
jithinnairs <33701182+jithinnairs@users.noreply.github.com>
2021-08-02 09:21:06 +0530

    Update SWLDraft.scala

--- maven-sample/src/test/scala/frontline/sample/SWLDraft.scala
+++ maven-sample/src/test/scala/frontline/sample/SWLDraft.scala
@@ -1,11 +1,12 @@
 
+package SWLLoadTest
+
+
 import scala.concurrent.duration._
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.gatling.jdbc.Predef._
-
-package SWLLoadTest
 
 
 class SWLDraft extends Simulation {
